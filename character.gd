3a476b6ab0dd0719ec30aa55367f6490511d2211
Zapzap <zuzannaa.pietrzak@gmail.com>
2025-02-16 16:00:56 +0100

    itemy fix
    nie działają już na podstawie sygnałów i probably działa poprawnie

--- character.gd
+++ character.gd
@@ -4,11 +4,13 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-#Item area mówi czy jakiś item jest w area iterakcji
-var item_area = false
 
 var the_pickable_item
+var item_area = false
 
+#do sortowania itemów
+func sort_by_index(a, b):
+			return a[1] < b[1]
 
 
 func _physics_process(delta: float) -> void:
@@ -18,9 +20,21 @@ func _physics_process(delta: float) -> void:
 	
 	# Interact/take items
 	if Input.is_action_just_pressed("interact"):
-		if item_area:
-			if the_pickable_item.is_in_group("pickable") and the_pickable_item.can_take:
-				the_pickable_item.taken()
+		#tworzy tabelę z itemami w zasięgu i sortuje je od najbliższego do najdalszego
+		var item_tab = []
+		#sprawdza czy w tej tabeli coś jest
+		print($interactable_area.get_overlapping_bodies())
+		for i in $interactable_area.get_overlapping_bodies():
+			if i.is_in_group("pickable"):
+				item_tab.append([i,position.distance_to(i.position)])
+		item_tab.sort_custom(sort_by_index)
+		print(item_tab)
+		#items_nears = sorted($interactable_area.get_overlapping_bodies(), key=$charcter.position.distance_to(item.position))
+		if len(item_tab) >0:
+			the_pickable_item = item_tab[0][0]
+			print(the_pickable_item)
+			if the_pickable_item.can_take:
+					the_pickable_item.taken()
 		
 	
 	# Handle jump.
@@ -45,13 +59,3 @@ func _physics_process(delta: float) -> void:
 	#Camera control is for this to be linked to camera, Camera pos is for offset, and cameras are to see
 	
 	#testowa animacja żeby działała w otworzeniu
-	
-
-func item_area_entered(body: Node3D) -> void:
-	the_pickable_item = body
-	item_area = true
-
-func item_area_exit(_body: Node3D) -> void:
-	item_area = false
-	
-	
--- example_item.gd
+++ example_item.gd
@@ -1,7 +1,7 @@
 extends StaticBody3D
 #can_take - checks if item is still on the ground (and not alr picked up)
 var can_take = true
-
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 
 func taken():
 	can_take = false
