36ad181d7df42a6805e530269eb3d8425ed4dfd0
Zapzap <zuzannaa.pietrzak@gmail.com>
2025-02-26 09:54:05 +0100

    poprawa kamery
    naprawia ruch kamery żeby pasował do 2.5D

--- character.gd
+++ character.gd
@@ -7,7 +7,11 @@ const JUMP_VELOCITY = 4.5
 
 var the_pickable_item
 var item_area = false
-
+#zmienne służące do sprawdzania ruchu postaci na osi z żeby dostosowywać się do ruchu
+var do_kam_1 
+var do_kam_2
+var do_kam_diff
+var kamz = position.z
 #do sortowania itemów
 func sort_by_index(a, b):
 			return a[1] < b[1]
@@ -37,7 +41,7 @@ func _physics_process(delta: float) -> void:
 			if the_pickable_item.can_take:
 					the_pickable_item.taken()
 		
-	
+	do_kam_1 = position.z 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -46,6 +50,7 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("go_left", "go_right", "go_up", "go_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -54,8 +59,16 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
-	#Camera smoothing based on a yt tutorialsssssss
-	$Camera_control.position = lerp($Camera_control.position, position, 0.08)
+	
+	#to wszystko wyłapuje jak daleko ruszałaby się kamera gdyby ruszała się z kamerą
+	do_kam_2 = position.z
+	do_kam_diff = do_kam_1 - do_kam_2 
+	#mnożnik żeby ogarnąć jak mały ma być efekt kamery ruchem do przodu i do tyłu 
+	kamz = kamz - (do_kam_diff * 0.25)
+	$Camera_control.position.z = lerp($Camera_control.position.z, kamz, 0.08)
+	#Camera smoothing based on a yt tutorialsssssss/
+	$Camera_control.position.x = lerp($Camera_control.position.x, position.x, 0.08)
+	$Camera_control.position.y = lerp($Camera_control.position.y, position.y, 0.08)
 	#ps: the other camera in camera control is purely for "hey this is kinda cool" purpose
 	#Camera control is for this to be linked to camera, Camera pos is for offset, and cameras are to see
 	
