16de6f90321e5440aa22be1480796e26c9ce1e40
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2025-03-12 18:15:23 +0100

    Reading conversation info from file

--- ui/HUD/npc_dialog/npc_dialog.gd
+++ ui/HUD/npc_dialog/npc_dialog.gd
@@ -1,42 +1,47 @@
 extends MarginContainer
 
 var convo_data : Dictionary
-var current_state : int = 0
+var current_state : String = "0"
 var answers : Dictionary
 
 func _ready() -> void:
-	var dict : Dictionary
-	dict[0] = {}
-	dict[0]["text"] = "hello"
-	dict[0]["answers"] = {}
-	dict[0]["answers"][0] = {}
-	dict[0]["answers"][0]["text"] = "hello"
-	dict[0]["answers"][0]["next_id"] = 1
-	dict[0]["answers"][1] = {}
-	dict[0]["answers"][1]["text"] = "fuck u"
-	dict[0]["answers"][1]["next_id"] = 2
-	dict[1] = {}
-	dict[1]["text"] = "bye bye"
-	dict[1]["answers"] = {}
-	dict[1]["answers"][0] = {}
-	dict[1]["answers"][0]["text"] = "ok, bye..."
-	dict[1]["answers"][0]["next_id"] = -1
-	dict[2] = {}
-	dict[2]["text"] = "well fuck u too!"
-	dict[2]["answers"] = {}
-	dict[2]["answers"][0] = {}
-	dict[2]["answers"][0]["text"] = "..."
-	dict[2]["answers"][0]["next_id"] = -1
-	initialize(dict)
-	var file = FileAccess.open("res://ui/HUD/npc_dialog/example_dialog.json", FileAccess.WRITE)
-	file.store_string(JSON.stringify(dict, "\t"))
+	if convo_data.is_empty():
+		var file = FileAccess.open("res://ui/HUD/npc_dialog/example_dialog.json", FileAccess.READ)
+		var json_string = file.get_as_text()
+		convo_data = JSON.parse_string(json_string)
+		next(current_state)
+	#var dict : Dictionary
+	#dict[0] = {}
+	#dict[0]["text"] = "hello"
+	#dict[0]["answers"] = {}
+	#dict[0]["answers"][0] = {}
+	#dict[0]["answers"][0]["text"] = "hello"
+	#dict[0]["answers"][0]["next_id"] = 1
+	#dict[0]["answers"][1] = {}
+	#dict[0]["answers"][1]["text"] = "fuck u"
+	#dict[0]["answers"][1]["next_id"] = 2
+	#dict[1] = {}
+	#dict[1]["text"] = "bye bye"
+	#dict[1]["answers"] = {}
+	#dict[1]["answers"][0] = {}
+	#dict[1]["answers"][0]["text"] = "ok, bye..."
+	#dict[1]["answers"][0]["next_id"] = -1
+	#dict[2] = {}
+	#dict[2]["text"] = "well fuck u too!"
+	#dict[2]["answers"] = {}
+	#dict[2]["answers"][0] = {}
+	#dict[2]["answers"][0]["text"] = "..."
+	#dict[2]["answers"][0]["next_id"] = -1
+	#initialize(dict)
+	#var file = FileAccess.open("res://ui/HUD/npc_dialog/example_dialog.json", FileAccess.WRITE)
+	#file.store_string(JSON.stringify(dict, "\t"))
 
 # it should modify the convo data to be correct according to owned items
 func initialize(all_convo_data : Dictionary):#, owned_items : Array[Item]):
 	convo_data = all_convo_data
 	next(current_state)
 
-func next(id : int):
+func next(id : String):
 	current_state = id
 	if !convo_data.has(current_state):
 		print("convo finished")
@@ -57,4 +62,4 @@ func next(id : int):
 
 func _on_answer_gui_input(event: InputEvent, next_id : int = 0) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
-		next(next_id)
+		next(str(next_id))
