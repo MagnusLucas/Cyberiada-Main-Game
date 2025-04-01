4180662c8e1d9189a3f1b03f5285ef9bea6821aa
Zapzap <zuzannaa.pietrzak@gmail.com>
2025-04-01 02:21:37 +0200

    passage działa
    i hope
    also dodanie test_level_2 i usawienie przykładowego tekstu w środku obu testowych leveli

--- world/objects/passage.gd
+++ world/objects/passage.gd
@@ -1,13 +1,22 @@
 extends Node3D
-@export var next_scene : PackedScene
+@export_file("*.tscn") var next_scene : String
+#@export var next_scene : String
 #CLASA NEXT SCENE JESZCZE NIE DZIAŁA i chuj wie czemu nie
+var can_take : bool
+func _ready() -> void:
+	can_take = true
 
-var can_take = true
 func taken():
 	#evar next_scene := preload("res://world/places/test_level.tscn")
-	var nexter = load("res://world/places/test_level.tscn")
-	print('chuj')
-	can_take = false
+	var nexter = load(next_scene)
+	#print(stara,next_scene.resource_path)
+	#print(self.next_scene.path)
+	#print(self.next_scene.resource_path)
+	#nexter = self.next_scene
+	#print('chuj')
+	#nagle zaczełø magicznie działa¢, czemu hahahaha nie wiem
+	print(next_scene,' hohoho ', nexter)
+	#can_take = false
 	$AudioStreamPlayer3D.play()
-	print(self.next_scene)
+	#print(self.next_scene)
 	get_tree().change_scene_to_packed(nexter)
