8fd79d863e834ec5fc38ab12d6758b1302b732d0
Zapzap <zuzannaa.pietrzak@gmail.com>
2025-04-04 22:38:26 +0200

    ig pauza wróciła
    odhashtagowałam
    also próba dodania nowego detektywa zakończyła się porażką z przyczyn dochodzeniu nieznanym

--- game.gd
+++ game.gd
@@ -1,9 +1,9 @@
 extends Node3D
 
-#var pause = preload("res://ui/settings/in_game/pause.tscn")
+var pause = preload("res://ui/settings/in_game/pause.tscn")
 
 
-#func _input(event: InputEvent) -> void:
-	#if event is InputEventKey and event.keycode == KEY_ESCAPE and event.is_pressed():
-		#add_child(pause.instantiate())
-		#get_tree().paused = true
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.keycode == KEY_ESCAPE and event.is_pressed():
+		add_child(pause.instantiate())
+		get_tree().paused = true
