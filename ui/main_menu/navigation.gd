ff3fbbddf6af474175db5ee30ac0a04cb6af1bad
Regnihov <gapakiis@gmail.com>
2025-05-25 11:21:20 +0200

    Moved Krysia and quick-fixed main menu

--- ui/main_menu/navigation.gd
+++ ui/main_menu/navigation.gd
@@ -3,18 +3,21 @@ extends VBoxContainer
 
 func _on_quit_pressed() -> void:
 	$"../../../../AudioStreamPlayer".play()
+	$"../../../../loading_screen".show()
 	await $"../../../../AudioStreamPlayer".finished
 	get_tree().quit()
 
 
 func _on_load_pressed() -> void:
 	$"../../../../AudioStreamPlayer".play()
+	$"../../../../loading_screen".show()
 	await $"../../../../AudioStreamPlayer".finished
 	get_tree().change_scene_to_file("res://ui/saved_games/saved_games.tscn")
 
 
 func _on_settings_pressed() -> void:
 	$"../../../../AudioStreamPlayer".play()
+	$"../../../../loading_screen".show()
 	await $"../../../../AudioStreamPlayer".finished
 	get_tree().change_scene_to_file("res://ui/settings/settings/settings.tscn")
 
