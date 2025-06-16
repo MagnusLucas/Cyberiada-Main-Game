d970906b310deb360ab3103eebd60011d517ac6b
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2025-06-16 13:48:00 +0200

    Fixed sound in main menu

--- ui/main_menu/navigation.gd
+++ ui/main_menu/navigation.gd
@@ -2,28 +2,19 @@ extends VBoxContainer
 
 
 func _on_quit_pressed() -> void:
-	$"../../../../AudioStreamPlayer".play()
-	$"../../../../loading_screen".show()
-	await $"../../../../AudioStreamPlayer".finished
 	get_tree().quit()
 
 
 func _on_load_pressed() -> void:
-	$"../../../../AudioStreamPlayer".play()
-	$"../../../../loading_screen".show()
-	await $"../../../../AudioStreamPlayer".finished
+	Sound.play()
 	get_tree().change_scene_to_file("res://ui/saved_games/saved_games.tscn")
 
 
 func _on_settings_pressed() -> void:
-	$"../../../../AudioStreamPlayer".play()
-	$"../../../../loading_screen".show()
-	await $"../../../../AudioStreamPlayer".finished
+	Sound.play()
 	get_tree().change_scene_to_file("res://ui/settings/settings/settings.tscn")
 
 
 func _on_new_game_pressed() -> void:
-	$"../../../../AudioStreamPlayer".play()
-	$"../../../../loading_screen".show()
-	await $"../../../../AudioStreamPlayer".finished
+	Sound.play()
 	get_tree().change_scene_to_file("res://ui/prolog/prolog.tscn")
