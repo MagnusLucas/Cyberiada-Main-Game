78069bfb21958637c2d50e5edc899ae9e0919d06
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2025-06-22 22:30:07 +0200

    Going back to main menu from saved games

--- UI/menu/saved_games.gd
+++ UI/menu/saved_games.gd
@@ -2,7 +2,7 @@ extends HBoxContainer
 
 
 func _on_back_pressed() -> void:
-	get_tree().change_scene_to_file("res://ui/main_menu/main_menu.tscn")
+	get_tree().change_scene_to_file("res://UI/menu/main_menu.tscn")
 
 
 func _on_play_pressed() -> void:
