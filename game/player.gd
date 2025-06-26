fdc3ed93c84c08a5345380772bd464491c7b965b
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2025-06-26 16:08:48 +0200

    Some camera shenanigans and you can enter the shop

--- game/player.gd
+++ game/player.gd
@@ -4,15 +4,12 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 
-var the_pickable_item
-var item_area = false
 #zmienne służące do sprawdzania ruchu postaci na osi z żeby dostosowywać się do ruchu
-var start_kam
 var do_kam_1 = position.z 
 var do_kam_2
-var do_kam_diff
 var kamz = position.z
-var zoom = 0
+var SceneCameraOffset = Vector3(0,0,4)
+
 #do sortowania itemów
 func sort_by_index(a, b):
 	return a[1] < b[1]
@@ -27,7 +24,6 @@ func _ready() -> void:
 	sound_timer.autostart = true
 	sound_timer.set_one_shot(true)
 	add_child(sound_timer)
-	start_kam = $Camera_control.position.z
 	
 func _get_closest_interactable() -> Area3D:
 	var interactable_tab = []
@@ -38,6 +34,8 @@ func _get_closest_interactable() -> Area3D:
 		return interactable_tab[0][0]
 	return null
 
+func reset_camera(z_offset : float) -> void:
+	$Camera_control/Camera_pos.position.z = z_offset
 
 func try_to_interact(interactable : Area3D) -> void:
 	if interactable is Item:
@@ -54,14 +52,7 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
-	# Interact/take items
-	if Input.is_action_just_pressed("interact"):
-		try_to_interact(_get_closest_interactable())
-	
-	
-	do_kam_1 = position.z 
 	# Handle jump.
-		
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		$AudioStreamPlayer.play()
 		velocity.y = JUMP_VELOCITY
@@ -79,56 +70,63 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	# Saving position before movement
+	do_kam_1 = position.z 
+	
+	# Moving the character
+	if is_inside_tree():
+		move_and_slide()
+	
+	# Adjusting camera
+	#to wszystko wyłapuje jak daleko ruszałaby się kamera gdyby ruszała się z kamerą
+	do_kam_2 = position.z
+	var do_kam_diff = do_kam_1 - do_kam_2 
+	#mnożnik żeby ogarnąć jak mały ma być efekt kamery ruchem do przodu i do tyłu 
+	kamz = kamz - (do_kam_diff * 0.25)
+	#Camera smoothing based on a yt tutorialsssssss/
+	const CAMERA_WEIGHT = 0.06
+	$Camera_control.position.z = lerp($Camera_control.position.z, kamz, CAMERA_WEIGHT)
+	$Camera_control.position.x = lerp($Camera_control.position.x, position.x, CAMERA_WEIGHT)
+	$Camera_control.position.y = lerp($Camera_control.position.y, position.y, CAMERA_WEIGHT)
+	#ps: the other camera in camera control is purely for "hey this is kinda cool" purpose
+	#Camera control is for this to be linked to camera, Camera pos is for offset, and cameras are to see
+	
+	
+	
+	# Character animation
 	if not is_on_floor():
-		#warunek do pierwszej pozycji skoku
-		#if velocity.y >0:
 		$Detektyw/AnimationPlayer.play("Tpose")
 	elif velocity.x !=0 or velocity.z != 0:
 		$Detektyw/AnimationPlayer.play("Walking")
-		#dźwięk contuwued
-		#print($AudioStreamPlayer.playing, sound_timer.time_left == 0, sound_timer.time_left)
 		if $AudioStreamPlayer.playing == false and sound_timer.time_left == 0:
 			var audio_path = "res://game/audio/SFX/footsteps/krok_"+str(randi_range(1,4))+".wav"
 			$AudioStreamPlayer.stream = load(audio_path)
 			sound_timer.start(randf_range(0.5,0.5)) 
 			$AudioStreamPlayer.play()
-		
 	else:
 		$Detektyw/AnimationPlayer.play("Standing")
-			
 	
-		
-		
 	
-	if is_inside_tree():
-		move_and_slide()
+	# Interact/take items
+	if Input.is_action_just_pressed("interact"):
+		try_to_interact(_get_closest_interactable())
 	
-	#to wszystko wyłapuje jak daleko ruszałaby się kamera gdyby ruszała się z kamerą
-	do_kam_2 = position.z
-	do_kam_diff = do_kam_1 - do_kam_2 
-	#mnożnik żeby ogarnąć jak mały ma być efekt kamery ruchem do przodu i do tyłu 
-	kamz = kamz - (do_kam_diff * 0.25)
-	$Camera_control.position.z = lerp($Camera_control.position.z, kamz + 6 + zoom, 0.08)
-	#Camera smoothing based on a yt tutorialsssssss/
-	$Camera_control.position.x = lerp($Camera_control.position.x, position.x, 0.08)
-	$Camera_control.position.y = lerp($Camera_control.position.y, position.y, 0.08)
-	#ps: the other camera in camera control is purely for "hey this is kinda cool" purpose
-	#Camera control is for this to be linked to camera, Camera pos is for offset, and cameras are to see
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		var zoom_limit = (self.position.z - start_kam) * (-1)
 		if event.is_pressed():
+			var camera = $Camera_control/Camera_pos/Camera3D
+			const MIN_ZOOM = 100
+			const MAX_ZOOM = 20
+			
+			var zoom_scale:float = 5 * (event.factor if event.factor else 1.0)
 			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
-				print(zoom_limit, ' ',zoom)
-				var zoom_scale:float = 2.0 ** (-event.factor if event.factor else 1.0)
-				if zoom <= zoom_limit:
-					zoom += zoom_scale
-				
+				if camera.fov + zoom_scale < MIN_ZOOM:
+					camera.fov += zoom_scale
 
 			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-				var zoom_scale:float = 2.0 ** (-event.factor if event.factor else -1.0)
 				#jeżeli zoom jest większy od limitu
-				if zoom >= zoom_limit * (-2):
+				if camera.fov - zoom_scale > MAX_ZOOM:
 					#zmniejsza zooma aka przybliża
-					zoom -= zoom_scale
+					camera.fov -= zoom_scale
