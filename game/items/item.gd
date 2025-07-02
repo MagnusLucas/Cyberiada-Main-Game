07d8c86e941bd0306ff9dce4f2da050a8af4a8e0
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2025-07-02 23:05:45 +0200

    Items can give clues

--- game/items/item.gd
+++ game/items/item.gd
@@ -4,12 +4,24 @@ class_name Item
 
 @export_file(".png") var texture = "res://game/items/2d/example_item.png"
 @export_file(".wav") var pickup_sound = "res://game/audio/SFX/pstryczek.wav"
+@export var pickable : bool = true
+
+@export_group("Notebook info on interaction")
+@export_enum("diary", "inhabitants", "clues") var notebook_category : String = "clues"
+@export var category_value : String = "example_item"
+@export var info : String = "This is an example of speech"
+
+
 
 func _ready() -> void:
 	var material : StandardMaterial3D = $MeshInstance3D.get_surface_override_material(0)
 	material.albedo_texture = load(texture)
 
 func take():
+	if info:
+		if !Notebook.data[notebook_category][category_value].has("info"):
+			Notebook.data[notebook_category][category_value]["info"] = []
+		Notebook.data[notebook_category][category_value]["info"].append(info)
 	if len(character.inv) <=5:
 		character.inv.append(name)
 		Sound.play_sfx(pickup_sound)
